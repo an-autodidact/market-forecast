1b8b8ccdd752daab68ad0a59e220339f46d2ceb2
Aman ₿📈🚀🌕 <84115108+an-autodidact@users.noreply.github.com>
2025-09-28 23:07:10 +0530

    Added License to About section

---
+++ libs /readme.py
@@ -0,0 +1,24 @@
+import os
+import inspect
+import streamlit as st
+
+
+def show_readme():
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+    markdown_file_path = parent_dir + "/README.md"
+    license_file_path = parent_dir + "/LICENSE"
+
+    if not os.path.exists(markdown_file_path):
+        st.error("Markdown file does not exist.")
+        st.stop()
+
+    with open(markdown_file_path, 'r', encoding="utf-8") as outfile:
+        markdown_content = outfile.read()
+
+    with open(license_file_path, 'r', encoding="utf-8") as outfile:
+        license_content = outfile.read()
+
+    st.markdown(markdown_content, unsafe_allow_html=True)
+    st.header("License")
+    st.markdown(license_content, unsafe_allow_html=True)
